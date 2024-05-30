2d2b7dc916810997b90f08c293acf41e927c3367
RedHat45 <anointed.hinds.30@gmail.com>
2024-05-30 19:59:57 -0400

    revised code

--- version.tf
+++ version.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.61.0"
+      version =  "4.61.0"
     }
   }
 }
 provider "aws" {
-  region = var.REGION
+  region = "us-east-1"
 
 }
